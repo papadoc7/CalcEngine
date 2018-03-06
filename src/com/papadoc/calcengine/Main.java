9c2158661b93ec185e3ef6a5581988e13f6265bc
Georgios <georgios.papadakis@cerner.com>
2018-03-06 08:43:12 +0000

    adding CalculateHelper - MathCommand

--- src/com/papadoc/calcengine/Main.java
+++ src/com/papadoc/calcengine/Main.java
@@ -80,6 +80,7 @@ public class Main {
 //        System.out.println(testEquation.getResult());
         
         
+/*
         MathEquation[] equations = new MathEquation[4];
         //with next lines we are using the constructor to create all the array elements
         equations[0] = new MathEquation('d', 100.0d, 50.0d );
@@ -137,8 +138,33 @@ public class Main {
             System.out.print("result=");
             System.out.println(calculator.getResult());
         }
-        
+*/
+
+       //useMathEquation(;
+       //useCalculatorBase();
+       
+       String[] statements = {
+           "divide 100.0 50.0", //100.0 / 50.0 = 2.0
+           "add 25.0 92.0", //25.0 + 92.0 = 117.0
+           "subtract 225.0 17.0", //225.0 - 17.0 = 108.0
+           "multiply 11.0 3.0", //11.0 * 3.0 = 33.0
+       };
+       
+       CalculateHelper helper = new CalculateHelper();
+       for(String statement:statements) {
+           helper.process(statement);
+           System.out.println(helper);
+       }
+       
     } //main-psvm
+    
+//    static void useMathEquation() {
+//        MathEquation[] equations = new MathEquation[4];
+//        equations[0] = new MathEquation('d', 100.0d, 50.0d );
+//        equations[1] = new MathEquation('a', 25.0d, 92.0d );
+//        equations[2] = new MathEquation('s', 225.0d, 17.0d);
+//        equations[3] = new MathEquation('m', 11.0d, 3.0d);        
+//    }
 
 //    public static MathEquation create(double leftVal, double rightVal, char opCode) {
 //        MathEquation equation = new MathEquation();
